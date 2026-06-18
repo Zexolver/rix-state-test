0d7d9021131f88e1ac288697e4829eddd541b4a9
Rix Optimizer <rix@localhost>
2026-06-18 00:10:19 -0400

    rix: initialized environment

--- groups/upstream/default.nix
+++ groups/upstream/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, peeky, swiftfetch, ... }:
+{ pkgs, quantumlauncher, peeky, swiftfetch, ... }:
 [
   # Packages managed by Rix
   pkgs.coreutils # Installed via Rix
   pkgs.__ext_flake or (swiftfetch.packages.${pkgs.system}.default) # External flake: github:Ly-sec/swiftfetch
   pkgs.__ext_flake or (peeky.packages.${pkgs.system}.default) # External flake: github:dmyTRUEk/peeky
+  pkgs.__ext_flake or (quantumlauncher.packages.${pkgs.system}.default) # External flake: github:Mrmayman/quantumlauncher
+  pkgs.dorion # Installed via Rix
 ]
