f1a360a317af840c130268269f8aa1937eb19686
Rix System Manager <rix@localhost>
2026-06-19 12:22:15 -0400

    chore: automated Rix environment update

--- groups/upstream/default.nix
+++ groups/upstream/default.nix
@@ -10,4 +10,5 @@
   pkgs.bat # Installed via Rix
   pkgs.__ext_flake or (webx.packages.${pkgs.system}.default or webx.defaultPackage.${pkgs.system} or webx.packages.${pkgs.system}.webx) # External flake: github:face-hh/webx
   pkgs.genact # Installed via Rix
+  pkgs.oxker # Installed via Rix
 ]
