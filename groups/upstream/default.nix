9f3d7820248a2386451504759d9e997c992aef12
Rix System <rix@local>
2026-06-19 00:19:21 -0400

    chore: automated Rix environment update

--- groups/upstream/default.nix
+++ groups/upstream/default.nix
@@ -9,4 +9,5 @@
   pkgs.__ext_flake or (xplr.packages.${pkgs.system}.default or xplr.defaultPackage.${pkgs.system} or xplr.packages.${pkgs.system}.xplr) # External flake: github:sayanarijit/xplr
   pkgs.bat # Installed via Rix
   pkgs.__ext_flake or (webx.packages.${pkgs.system}.default or webx.defaultPackage.${pkgs.system} or webx.packages.${pkgs.system}.webx) # External flake: github:face-hh/webx
+  pkgs.genact # Installed via Rix
 ]
