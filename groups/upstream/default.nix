88bede9d64744b07dc002717ece8ea58d258b55e
Rix System Manager <rix@localhost>
2026-06-30 12:37:37 -0400

    rix: optimize environment state and sync configurations

--- groups/upstream/default.nix
+++ groups/upstream/default.nix
@@ -4,11 +4,12 @@
   pkgs.coreutils # Installed via Rix
   pkgs.__ext_flake or (swiftfetch.packages.${pkgs.system}.default) # External flake: github:Ly-sec/swiftfetch
   pkgs.__ext_flake or (peeky.packages.${pkgs.system}.default) # External flake: github:dmyTRUEk/peeky
-  pkgs.dorion # Installed via Rix
-  pkgs.netscanner # Installed via Rix
+pkgs.netscanner # Installed via Rix
   pkgs.__ext_flake or (xplr.packages.${pkgs.system}.default or xplr.defaultPackage.${pkgs.system} or xplr.packages.${pkgs.system}.xplr) # External flake: github:sayanarijit/xplr
   pkgs.bat # Installed via Rix
   pkgs.__ext_flake or (webx.packages.${pkgs.system}.default or webx.defaultPackage.${pkgs.system} or webx.packages.${pkgs.system}.webx) # External flake: github:face-hh/webx
   pkgs.genact # Installed via Rix
   pkgs.oxker # Installed via Rix
+  pkgs.wezterm # Installed via Rix
+  pkgs.mullvad-browser # Installed via Rix
 ]
